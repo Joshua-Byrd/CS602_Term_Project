dc5348287a06114c1261a127d8c9216b7f5ade34
Joshua Byrd <60490767+Joshua-Byrd@users.noreply.github.com>
2024-02-10 09:07:38 -0500

    Added pages to display read and want-to-read lists

--- seed.js
+++ seed.js
@@ -64,6 +64,24 @@ async function seed() {
       ],
     },
     {
+      title: "War of the Worlds",
+      author: ["H. G. Wells"],
+      description:
+        "The plot is similar to other works of invasion literature from the same period, and has been variously interpreted as a commentary on the theory of evolution, British colonialism, and Victorian-era fears, superstitions and prejudices. Wells later noted that inspiration for the plot was the catastrophic effect of European colonisation on the Aboriginal Tasmanians. Some historians have argued that Wells wrote the book to encourage his readership to question the morality of imperialism. At the time of publication, it was classified as a scientific romance, like Wells's earlier novel, The Time Machine.",
+      isbn13: "978-0866118705",
+      isbn10: "",
+      genre: ["Science Fiction Adventures", "Classic Literature & Fiction"],
+      releaseYear: 1897,
+      ratings: [3, 2.5, 2, 4, 4.5],
+      reviews: [
+        { user: "bookLover", review: "The first alien invasion novel" },
+        {
+          user: "i_read_classics",
+          review: "'salright",
+        },
+      ],
+    },
+    {
       title: "Fight Club",
       author: ["Chuck Palahniuk"],
       description:
@@ -78,6 +96,48 @@ async function seed() {
         {
           user: "johnQcitizen",
           review: "My favorite novel ever",
+        },
+      ],
+    },
+    {
+      title: "Choke",
+      author: ["Chuck Palahniuk"],
+      description:
+        "Victor Mancini, a medical-school dropout, is an antihero for our deranged times. Needing to pay elder care for his mother, Victor has devised an ingenious scam: he pretends to choke on pieces of food while dining in upscale restaurants. He then allows himself to be “saved” by fellow patrons who, feeling responsible for Victor's life, go on to send checks to support him. When he's not pulling this stunt, Victor cruises sexual addiction recovery workshops for action, visits his addled mom, and spends his days working at a colonial theme park. His creator, Chuck Palahniuk, is the visionary we need and the satirist we deserve.",
+      isbn13: "",
+      isbn10: "",
+      genre: ["Literary Satire Fiction", "Literature & Fiction"],
+      releaseYear: 2002,
+      ratings: [1, 3.5, 4.5, 3, 3],
+      reviews: [
+        {
+          user: "Agoodusername",
+          review: "Hilarious, cringe-inducing, awful, and great.",
+        },
+        {
+          user: "ChuckPFan",
+          review: "I still don't get it",
+        },
+      ],
+    },
+    {
+      title: "Haunted: A Novel",
+      author: ["Chuck Palahniuk"],
+      description:
+        "Haunted is a novel made up of twenty-three horrifying, hilarious, and stomach-churning stories. They're told by people who have answered an ad for a writer's retreat and unwittingly joined a “Survivor”-like scenario where the host withholds heat, power, and food. As the storytellers grow more desperate, their tales become more extreme, and they ruthlessly plot to make themselves the hero of the reality show that will surely be made from their plight. This is one of the most disturbing and outrageous books you'll ever read, one that could only come from the mind of Chuck Palahniuk.",
+      isbn13: "",
+      isbn10: "9780099458371",
+      genre: ["Horror", "Literature & Fiction"],
+      releaseYear: 2005,
+      ratings: [1, 5, 4.5, 5, 2],
+      reviews: [
+        {
+          user: "iFightUnderground",
+          review: "The most disgusting book I've ever read",
+        },
+        {
+          user: "ScubaSteve",
+          review: "Wonderfully awful",
         },
       ],
     },
@@ -142,6 +202,27 @@ async function seed() {
         },
       ],
     },
+    {
+      title: "Animal Farm",
+      author: ["George Orwell"],
+      description:
+        "A farm is taken over by its overworked, mistreated animals. With flaming idealism and stirring slogans, they set out to create a paradise of progress, justice, and equality. Thus the stage is set for one of the most telling satiric fables ever penned—a razor-edged fairy tale for grown-ups that records the evolution from revolution against tyranny to a totalitarianism just as terrible.",
+      isbn13: "",
+      isbn10: "9780451526342",
+      genre: [
+        "Dystopian Fiction",
+        "Classic Literature & Fiction",
+        "Literary Fiction",
+      ],
+      releaseYear: 1975,
+      ratings: [5, 3, 4.5, 4.5, 2],
+      reviews: [
+        {
+          user: "scifi_reader",
+          review: "Everyone's great till they get in power.",
+        },
+      ],
+    },
   ];
 
   //insert the books and return confirmation
@@ -151,97 +232,6 @@ async function seed() {
     })
     .catch((err) => {
       console.log("Something went wrong when inserting the books: ", err);
-    });
-
-  //Populate Authors------------------------------------------------------------
-  const authorSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    bio: String,
-    booksWritten: [String],
-  });
-
-  const Author = new mongoose.model("Author", authorSchema);
-
-  authorsToInsert = [
-    {
-      firstName: "H.G.",
-      lastName: "Wells",
-      bio: "Herbert George Wells (21 September 1866 - 13 August 1946) was an English writer. Prolific in many genres, he wrote more than fifty novels and dozens of short stories. His non-fiction output included works of social commentary, politics, history, popular science, satire, biography, and autobiography. Wells' science fiction novels are so well regarded that he has been called the 'father of science fiction'.",
-      booksWritten: [
-        "The Time Machine",
-        "The Wonderful Visit",
-        "The Island of Doctor Moreau",
-        "The Wheels of Chance",
-        "The Invisible Man",
-        "The War of the Worlds",
-        "When the Sleeper Wakes",
-        "Love and Mr Lewisham",
-        "The First Men in the Moon",
-      ],
-    },
-    {
-      firstName: "F. Scott",
-      lastName: "Fitzgerald",
-      bio: "(September 24, 1896 - December 21, 1940) was an American novelist, essayist, and short story writer. He is best known for his novels depicting the flamboyance and excess of the Jazz Age—a term he popularized in his short story collection Tales of the Jazz Age. During his lifetime, he published four novels, four story collections, and 164 short stories. Although he achieved temporary popular success and fortune in the 1920s, Fitzgerald received critical acclaim only after his death and is now widely regarded as one of the greatest American writers of the 20th century.",
-      booksWritten: [
-        "The Great Gatsby",
-        "This Side Of Paradise",
-        "The Beautiful And the Damned",
-        "Tender Is The Night",
-      ],
-    },
-    {
-      firstName: "Chuck",
-      lastName: "Palahniuk",
-      bio: "Charles Michael 'Chuck' Palahniuk (born February 21, 1962) is an American novelist who describes his work as transgressional fiction. He has published 19 novels, three nonfiction books, two graphic novels, and two adult coloring books, as well as several short stories. His first published novel was Fight Club, which was adapted into a film of the same title.",
-      booksWritten: [
-        "Fight club",
-        "Haunted",
-        "Lullaby",
-        "Invisible Monsters",
-        "Choke",
-        "Survivor",
-      ],
-    },
-    {
-      firstName: "Mallory",
-      lastName: "Pearson",
-      bio: "New author Mallory Pearson made her debut with this year's 'We Ate the Dark'.",
-      booksWritten: ["We Ate The Dark"],
-    },
-    {
-      firstName: "Cixin",
-      lastName: "Liu",
-      bio: "Liu Cixin is a Chinese computer engineer and science fiction writer.He is a nine-time winner of China's Galaxy Award and has also received the 2015 Hugo Award for his novel The Three-Body Problem as well as the 2017 Locus Award for Death's End. He is also a winner of the Chinese Nebula Award. In English translations of his works, his name is given as Cixin Liu. ",
-      booksWritten: [
-        "China 2185",
-        "The Devil's Bricks",
-        "Ball Lightning",
-        "The Three Body Problem",
-        "The Dark Forest",
-        "Death's End",
-      ],
-    },
-    {
-      firstName: "George",
-      lastName: "Orwell",
-      bio: "Eric Arthur Blair (25 June 1903 - 21 January 1950) was a British novelist, essayist, journalist, and critic who wrote under the name George Orwell. His work is characterised by lucid prose, social criticism, opposition to totalitarianism, and support of democratic socialism.",
-      booksWritten: [
-        "Animal Farm",
-        "Nineteen Eighty-Four",
-        "The Road to Wigan Pier",
-        "Home To Catlonia",
-      ],
-    },
-  ];
-
-  await Author.insertMany(authorsToInsert)
-    .then((authorsToInsert) => {
-      console.log(`${authorsToInsert.length} authors inserted into database.`);
-    })
-    .catch((err) => {
-      console.log("Something went wrong when inserting the authors: ", err);
     });
 
   //Populate Users--------------------------------------------------------------
